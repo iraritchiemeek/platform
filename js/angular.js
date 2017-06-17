5b6c843b7e0b78d8a5b8fd09a46ba744eeb043cf
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-06-17 14:58:13 +1200

    animation on circles

--- js/angular.js
+++ js/angular.js
@@ -271,7 +271,9 @@ platformApp.controller('mainController', function($scope) {
 		$('.title').hide()
 	}
 
-	$scope.highlightCircle = function(n) {
+	$scope.highlightCircle = function(n, $event) {
+		$('.brands-images__image, .brands span').removeAttr("style")
+		$($event.target).css({'margin-left': '-15px', 'color': '#555555'})
 		$('.brands-images__image:nth-child(' + n + ')').css({'background-image': 'url("../imgs/greyCircle' + n + '.png")'})
 	}
 
